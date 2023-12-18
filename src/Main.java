3a2f6fc2a377ab1acf09c25084df5059ef80681f
Isfar Babul <isfarb2@nycstudents.net>
2023-12-18 18:55:49 -0500

    height

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,11 @@ public class Main {
         System.out.println("**** it whatever i cant spell anyways, **** you isfar for not allowing me to make the joke.");
         System.out.println("What is 9 + 7");
         System.out.println("Fun fact: the mitochondria is the powerhouse of the cell.");
+        double height = 6.0;
+        if (height < 6.0) {
+            System.out.println("Sorry, not interested you have to be 6 feet.");
+        } else {
+            System.out.println("W Riz.");
+        }
     }
 }
