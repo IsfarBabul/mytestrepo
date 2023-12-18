48474a38ddc04b90b93bb95a890877d8fda4e3bf
Gabriel <hypixelnerd347@gmail.com>
2023-12-18 18:49:21 -0500

    i totally wasnt gonna put a swear word in here nope im a good holy boi

--- src/Calculus.java
+++ src/Calculus.java
@@ -1,8 +1,9 @@
 public class Calculus {
     private int num;
     private int derivative;
+    private int integral = 77;
     public Calculus(int num) {
         this.num = num;
-        derivative = 0;
+        derivative = 99;
     }
 }
