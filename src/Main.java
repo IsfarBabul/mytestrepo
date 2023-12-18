d47862e52a7dadd624e83e328cd69dad99f048b4
Gabriel <hypixelnerd347@gmail.com>
2023-12-18 18:51:09 -0500

    heheha

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Dimensional Meal was a hassle.");
         System.out.println("**** it whatever i cant spell anyways, **** you isfar for not allowing me to make the joke.");
         System.out.println("What is 9 + 7");
+        System.out.println("Fun fact: the mitochondria is the powerhouse of the cell.");
     }
 }
