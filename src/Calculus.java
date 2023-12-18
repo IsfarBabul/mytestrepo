5220fc6f9bc32647609bb80aafb911aaf65d43b8
Isfar Babul <isfarb2@nycstudents.net>
2023-12-18 18:51:43 -0500

    polarCoordinates

--- src/Calculus.java
+++ src/Calculus.java
@@ -2,8 +2,12 @@ public class Calculus {
     private int num;
     private int derivative;
     private int integral = 77;
+    private int polarCoordinateX;
+    private int polarCoordinateY;
     public Calculus(int num) {
         this.num = num;
         derivative = 99;
+        polarCoordinateX = 0;
+        polarCoordinateY = 0;
     }
 }
