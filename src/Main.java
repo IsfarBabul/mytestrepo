f9a6c7202b4062b696e98e1442b204cf3bd9fe2f
Gabriel <hypixelnerd347@gmail.com>
2023-12-18 19:00:39 -0500

    heheha

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Dimensional Meal was a hassle.");
         System.out.println("**** it whatever i cant spell anyways, **** you isfar for not allowing me to make the joke.");
-        System.out.println("What is 9 + 7");
+        System.out.println("What is your ip address?");
         System.out.println("Fun fact: the mitochondria is the powerhouse of the cell.");
         double height = 6.0;
         if (height < 6.0) {
