b66346b2bd96ed7c22b95865f7b95683c726aacb
Isfar Babul <isfarb2@nycstudents.net>
2023-12-18 19:06:11 -0500

    changed fact more

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         System.out.println("Dimensional Meal was a hassle.");
         System.out.println("**** it whatever i cant spell anyways, **** you isfar for not allowing me to make the joke.");
         System.out.println("What is your ip address?");
-        System.out.println("Fun fact: The hit song \"Never Gonna  Give You Up\" came out in 1987.");
+        System.out.println("Fun fact: The hit song \"Never Gonna  Give You Up\" by Rick Astley came out in 1987.");
         double height = 6.0;
         if (height < 6.0) {
             System.out.println("Sorry, not interested you're not 6 feet.");
